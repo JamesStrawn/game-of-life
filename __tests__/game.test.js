56a250ae71f318c2211255b2e807b2a4f5ff161d
Jim Strawn <JamesStrawn@lithia.com>
2023-06-06 15:34:54 -0700

    Added main functionality plus tests

--- __tests__/game.test.js
+++ __tests__/game.test.js
@@ -1,3 +1,88 @@
+const { createGrid, applyRules, countLiveNeighbors } = require("../game");
+
 test("Example Test", () => {
   expect(2 + 2).toBe(4);
+});
+
+test("Create Grid", () => {
+  const rows = 3;
+  const cols = 3;
+  const grid = createGrid(rows, cols);
+  expect(grid.length).toBe(rows);
+  expect(grid[0].length).toBe(cols);
+  expect(grid.every((row) => row.every((cell) => cell === 0))).toBe(true);
+});
+
+// Test all the boundary conditions for countLiveNeighbors
+test("Count Live Neighbors - NW", () => {
+  const grid = [
+    [0, 1, 0],
+    [1, 1, 0],
+    [0, 0, 0],
+  ];
+  const liveNeighbors = countLiveNeighbors(grid, 0, 0);
+  expect(liveNeighbors).toBe(3);
+});
+
+test("Count Live Neighbors - NE", () => {
+  const grid = [
+    [0, 1, 0],
+    [1, 1, 0],
+    [0, 0, 0],
+  ];
+  const liveNeighbors = countLiveNeighbors(grid, 0, 2);
+  expect(liveNeighbors).toBe(2);
+});
+
+test("Count Live Neighbors - SW", () => {
+  const grid = [
+    [0, 1, 0],
+    [1, 1, 0],
+    [0, 0, 0],
+  ];
+  const liveNeighbors = countLiveNeighbors(grid, 2, 0);
+  expect(liveNeighbors).toBe(2);
+});
+
+test("Count Live Neighbors - SE", () => {
+  const grid = [
+    [0, 1, 0],
+    [1, 1, 0],
+    [0, 0, 0],
+  ];
+  const liveNeighbors = countLiveNeighbors(grid, 2, 2);
+  expect(liveNeighbors).toBe(1);
+});
+
+test("Apply Rules", () => {
+  // Blinker pattern
+  const grid = [
+    [0, 1, 0],
+    [0, 1, 0],
+    [0, 1, 0],
+  ];
+  const newGrid = applyRules(grid);
+  expect(newGrid).toEqual([
+    [0, 0, 0],
+    [1, 1, 1],
+    [0, 0, 0],
+  ]);
+});
+
+test("applyRules calls printGrid", () => {
+  const grid = [
+    [0, 1, 0],
+    [1, 0, 1],
+    [0, 1, 0],
+  ];
+
+  const logSpy = jest.spyOn(console, "log");
+  const expectedOutput = ["⬜️ 🟩 ⬜️", "🟩 ⬜️ 🟩", "⬜️ 🟩 ⬜️"];
+
+  applyRules(grid);
+
+  expect(logSpy.mock.calls[0][0]).toBe("Generation: ");
+  expect(logSpy.mock.calls.slice(1).flat()).toEqual(expectedOutput);
+
+  logSpy.mockRestore();
 });
---
+++ game.js
@@ -0,0 +1,116 @@
+function createGrid(rows, cols) {
+  const grid = [];
+  for (let i = 0; i < rows; i++) {
+    const row = [];
+    for (let j = 0; j < cols; j++) {
+      row.push(0);
+    }
+    grid.push(row);
+  }
+  return grid;
+}
+
+/*
+function countLiveNeighbors(grid, row, col) {
+  let liveNeighbors = 0;
+
+  if (grid[row - 1] && grid[row - 1][col - 1] === 1) liveNeighbors++;
+  if (grid[row - 1] && grid[row - 1][col] === 1) liveNeighbors++;
+  if (grid[row - 1] && grid[row - 1][col + 1] === 1) liveNeighbors++;
+  if (grid[row][col - 1] === 1) liveNeighbors++;
+  if (grid[row][col + 1] === 1) liveNeighbors++;
+  if (grid[row + 1] && grid[row + 1][col - 1] === 1) liveNeighbors++;
+  if (grid[row + 1] && grid[row + 1][col] === 1) liveNeighbors++;
+  if (grid[row + 1] && grid[row + 1][col + 1] === 1) liveNeighbors++;
+
+  return liveNeighbors;
+}
+*/
+
+function countLiveNeighbors(grid, row, col) {
+  const rows = grid.length;
+  const cols = grid[0].length;
+  let count = 0;
+
+  // Define the neighboring cell offsets
+  const offsets = [
+    [-1, -1] /* NW */,
+    [-1, 0] /* N */,
+    [-1, 1] /* NE */,
+    [0, -1] /* W */,
+    [0, 1] /* E */,
+    [1, -1] /* SW */,
+    [1, 0] /* S */,
+    [1, 1] /* SE */,
+  ];
+
+  // Loop over the neighbors
+  for (const [offsetRow, offsetCol] of offsets) {
+    const neighborRow = row + offsetRow;
+    const neighborCol = col + offsetCol;
+
+    // Check if the neighbor cell is within the grid boundaries
+    if (
+      neighborRow >= 0 &&
+      neighborRow < rows &&
+      neighborCol >= 0 &&
+      neighborCol < cols
+    ) {
+      // Add the neighbor's value to the count
+      count += grid[neighborRow][neighborCol];
+    }
+  }
+
+  return count;
+}
+
+function applyRules(grid) {
+  const rows = grid.length;
+  const cols = grid[0].length;
+
+  const newGrid = [];
+
+  for (let i = 0; i < rows; i++) {
+    const newRow = [];
+    for (let j = 0; j < cols; j++) {
+      const cell = grid[i][j];
+      const liveNeighbors = countLiveNeighbors(grid, i, j);
+      let newCell;
+
+      if (cell === 1) {
+        if (liveNeighbors < 2 || liveNeighbors > 3) {
+          newCell = 0; // Cell dies due to under- or over-population
+        } else {
+          newCell = 1; // Cell lives on to next generation
+        }
+      } else {
+        if (liveNeighbors === 3) {
+          newCell = 1; // Cell is born - reproduction
+        } else {
+          newCell = 0; // Cell remains dead
+        }
+      }
+
+      newRow.push(newCell);
+    }
+    newGrid.push(newRow);
+  }
+
+  console.log("Generation: ");
+  printGrid(newGrid);
+
+  return newGrid;
+}
+
+function printGrid(grid) {
+  for (const row of grid) {
+    const rowString = row.map((cell) => (cell === 1 ? "🟩" : "⬜️")).join(" ");
+    console.log(rowString);
+  }
+}
+
+module.exports = {
+  createGrid,
+  applyRules,
+  countLiveNeighbors,
+};
